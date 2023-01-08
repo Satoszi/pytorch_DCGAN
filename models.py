05beda3325291cd2d3d5586acd42edf9b1a4fcde
Satoszi <stickyy6@gmail.com>
2023-01-08 03:00:54 +0100

    working DCGAN

--- models.py
+++ models.py
@@ -2,6 +2,34 @@ import torch
 import torch.nn as nn
 
 
+class Discriminator(nn.Module):
+    def __init__(self, filters):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            self._block(3         , 2*filters, 4, 2, 0, bias=False), # out 32x32 #Shouldn't be batchnorm
+            self._block(2*filters, 4*filters , 4, 2, 1, bias=False), # out 16x16
+            self._block(4*filters , 8*filters , 4, 2, 1, bias=False), # out 8x8
+            self._block(8*filters , 16*filters , 4, 2, 1, bias=False), # out 4x4
+            self._block(16*filters , 1   , 4, 2, 1, bias=False, out=True), # out 1x1 #Shouldn't be batchnorm
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding, bias, out=False):
+        return nn.Sequential(            
+            nn.Conv2d(in_channels=in_channels,
+                               out_channels=out_channels, 
+                               kernel_size=kernel_size,
+                               stride=stride,
+                               padding=padding, 
+                               bias=bias),
+            
+            nn.BatchNorm2d(out_channels),
+            nn.Sigmoid() if out else nn.LeakyReLU(0.2)
+            )
+        
+    def forward(self, x):
+        return self.disc( x)
+
+
 class Generator(nn.Module):
     def __init__(self,nz, filters):
         super(Generator, self).__init__()
@@ -30,32 +58,7 @@ class Generator(nn.Module):
         return self.gan( x)
 
 
-class Discriminator(nn.Module):
-    def __init__(self, filters):
-        super(Discriminator, self).__init__()
-        self.gan = nn.Sequential(
-            self._block(3         , 2*filters, 4, 2, 0, bias=False), # out 32x32 #Shouldn't be batchnorm
-            self._block(2*filters, 4*filters , 4, 2, 1, bias=False), # out 16x16
-            self._block(4*filters , 8*filters , 4, 2, 1, bias=False), # out 8x8
-            self._block(8*filters , 16*filters , 4, 2, 1, bias=False), # out 4x4
-            self._block(16*filters , 1   , 4, 2, 1, bias=False, out=True), # out 1x1 #Shouldn't be batchnorm
-        )
 
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding, bias, out=False):
-        return nn.Sequential(            
-            nn.Conv2d(in_channels=in_channels,
-                               out_channels=out_channels, 
-                               kernel_size=kernel_size,
-                               stride=stride,
-                               padding=padding, 
-                               bias=bias),
-            
-            nn.BatchNorm2d(out_channels),
-            nn.Sigmoid() if out else nn.LeakyReLU(0.2)
-            )
-        
-    def forward(self, x):
-        return self.gan( x)
 
 
 # def test():
